28015697d1af9a640f5d60c10396a0db357f39f9
Ilya <sobin_98@mail.ru>
2022-07-30 21:38:01 +0400

    final version

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -16,20 +16,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLight.layer.cornerRadius = 63
         redLight.alpha = 0.4
         redLight.backgroundColor = .red
         
-        yellowLight.layer.cornerRadius = 63
         yellowLight.alpha = 0.4
         yellowLight.backgroundColor = .yellow
         
-        greenLight.layer.cornerRadius = 63
         greenLight.alpha = 0.4
         greenLight.backgroundColor = .green
         
         bottonView.layer.cornerRadius = 20
         bottonView.backgroundColor = .white
+    }
+    
+    override func viewDidLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width/2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width/2
+        greenLight.layer.cornerRadius = greenLight.frame.width/2
     }
 
     @IBAction func bottonAction() {
